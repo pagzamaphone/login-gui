aa4f25f028ffa5fde81328883b2e9ae7609f07ea
Johnny Console <johnny.console@hotmail.com>
2018-02-10 11:35:49 -0500

    Completed the LoginGUI Class

--- LoginGUI/src/LoginGUI.java
+++ LoginGUI/src/LoginGUI.java
@@ -3,8 +3,11 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -13,31 +16,37 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-@SuppressWarnings({"serial","resource"})
+@SuppressWarnings({ "serial" })
 public class LoginGUI extends JFrame {
 
+	//Public Static variables for use with other program classes
 	public static File file = new File("credentials.txt");
 	public static String[] users = new String[10];
 	public static String type;
 	
-
+	//Private Static variables to use all over this program class
+	private static Scanner fileScanner; 
 	private JTextField user;
 	private JTextField pass;
-	
+
+	//Constructor for the LoginGUI
 	public LoginGUI() throws IOException {
-		for(int i = 0; i < users.length; i++) {
+		//Fill the array with empty strings to avoid a NullPointerExcetion later
+		for (int i = 0; i < users.length; i++) {
 			users[i] = "";
 		}
-		if(file.createNewFile()) {
+		
+		//If the credentials file needs to be created, ask the user for a username and password to set up.
+		if (file.createNewFile()) {
 			JPanel panel = new JPanel();
-			panel.setLayout(new GridLayout(2,2));
+			panel.setLayout(new GridLayout(2, 2));
 			user = new JTextField();
 			pass = new JTextField();
 			JButton ok = new JButton("Confirm");
 			ok.addActionListener(new InitializeListener());
 			add(ok, BorderLayout.SOUTH);
 			panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
-			panel.add(new JLabel("Enter a username: "),BorderLayout.WEST);
+			panel.add(new JLabel("Enter a username: "), BorderLayout.WEST);
 			panel.add(user, BorderLayout.EAST);
 			panel.add(new JLabel("Enter a password: "), BorderLayout.WEST);
 			panel.add(pass, BorderLayout.EAST);
@@ -45,13 +54,21 @@ public class LoginGUI extends JFrame {
 			setLocationRelativeTo(null);
 			setTitle("Initial Configuration - User");
 			setDefaultCloseOperation(EXIT_ON_CLOSE);
-			setSize(275,120);
+			setSize(275, 120);
 			setVisible(true);
-			
-		}
+
+		} 
+		
+		//If the credentials file exists, import the file's contents and store in the array for using, then ask the user to log in.
 		else {
+			try {
+				fileScanner = new Scanner(file);
+				for(int i = 0;  i < users.length && fileScanner.hasNextLine(); i++) {
+					users[i] = fileScanner.nextLine();
+				}
+			
 			JPanel panel = new JPanel();
-			panel.setLayout(new GridLayout(2,2));
+			panel.setLayout(new GridLayout(2, 2));
 			user = new JTextField();
 			pass = new JTextField();
 			JPanel button = new JPanel();
@@ -60,10 +77,10 @@ public class LoginGUI extends JFrame {
 			button.add(ok);
 			button.add(cancel);
 			cancel.addActionListener(e -> this.dispose());
-			//ok.addActionListener(new LoginListener());
+			ok.addActionListener(new LoginListener());
 			add(button, BorderLayout.SOUTH);
 			panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
-			panel.add(new JLabel("Enter username: "),BorderLayout.WEST);
+			panel.add(new JLabel("Enter username: "), BorderLayout.WEST);
 			panel.add(user, BorderLayout.EAST);
 			panel.add(new JLabel("Enter password: "), BorderLayout.WEST);
 			panel.add(pass, BorderLayout.EAST);
@@ -71,36 +88,58 @@ public class LoginGUI extends JFrame {
 			setLocationRelativeTo(null);
 			setTitle("Log In");
 			setDefaultCloseOperation(EXIT_ON_CLOSE);
-			setSize(275,120);
+			setSize(275, 120);
 			setVisible(true);
+			}
+			catch(FileNotFoundException e) {}
 		}
 	}
+
+	//Method to print the admin username once configured
 	private void repopFile() throws IOException {
 		PrintWriter writer = new PrintWriter(file.getName());
-		for(int i = 0; i < users.length; i++) {
-			if(users[i].equals("")) {
-				continue;
+		writer.println(users[0]);
+		writer.close();
+	}
+
+	//Listener for the initialization
+	private class InitializeListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			if (user.getText().equals("") || pass.getText().equals("")) {
+
+			} 
+			else {
+				users[0] = user.getText() + ":" + pass.getText() + ";admin";
+
+				LoginGUI.this.dispose();
+				try {
+					repopFile();
+					new LoginGUI();
+				}
+				catch (IOException e) {}
 			}
-			writer.println(users[i]);
 		}
 	}
 	
-	private class InitializeListener implements ActionListener {
-			@Override
-			public void actionPerformed(ActionEvent arg0){
-				if(user.getText().equals("") || pass.getText().equals("")) {
-					
-				}
-				else {
-				users[0] = user.getText() + ":" + pass.getText() + ";admin";
-				
-				LoginGUI.this.dispose();
-					try {
-						repopFile();
-						new LoginGUI();
-					} catch (IOException e) {}
+	//Listener for the login
+	private class LoginListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			for(int i = 0; i < users.length; i++) {
+				if(Arrays.toString(users).contains(user.getText() + ":" + pass.getText() + ";admin") ||
+					Arrays.toString(users).contains(user.getText() + ":" + pass.getText() + ";reg")) {
+					if(users[i].equals(user.getText() + ":" + pass.getText() + ";admin")) {
+						type = "admin";
+						//Add the Program Call Here
+					}
+					else if(users[i].equals(user.getText() + ":" + pass.getText() + ";reg")) {
+						type = "user";
+						//Add the Program Call Here
+					}
 				}
 			}
+		}
 	}
 }
-
